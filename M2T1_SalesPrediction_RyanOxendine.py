bddac69fc938d898f8cd22d8eb5bc20ecda95f71
oxendinr4243 <oxendinr4243@student.faytechcc.edu>
2017-06-08 23:12:27 -0400

    Add files via upload
    module 02

---
+++ M2T1_SalesPrediction_RyanOxendine.py
@@ -0,0 +1,15 @@
+#Sales prediction PG.77 
+#June 8, 2017
+#CTI-110- sales Prediction
+#Ryan Oxendine
+
+
+
+total_sales = float(input('Enter the Projected sales: '))
+
+# Calculate the profit ad a 23 percent of total sales
+profit = total_sales * .23
+
+# Display the profit
+print('the profit is $', format(profit, ',.2f'))
+
