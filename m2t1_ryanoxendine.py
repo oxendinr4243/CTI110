272ae5b7d850463bc69e062642762c779f7ccc9c
oxendinr4243 <oxendinr4243@student.faytechcc.edu>
2017-06-08 23:11:10 -0400

    Add files via upload
    module two

---
+++ m2t1_ryanoxendine.py
@@ -0,0 +1,29 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/Melinda/Desktop/M2T1_SalesPrediction_RyanOxendine.py ===
+Enter the Projected sales: 1000
+the profit is $ 230.0
+>>> 
+=== RESTART: C:/Users/Melinda/Desktop/M2T1_SalesPrediction_RyanOxendine.py ===
+Enter the Projected sales: 1000
+Traceback (most recent call last):
+  File "C:/Users/Melinda/Desktop/M2T1_SalesPrediction_RyanOxendine.py", line 14, in <module>
+    print('the profit is $', format(print, ',.2f'))
+TypeError: unsupported format string passed to builtin_function_or_method.__format__
+>>> 
+=== RESTART: C:/Users/Melinda/Desktop/M2T1_SalesPrediction_RyanOxendine.py ===
+Enter the Projected sales: 1000
+Traceback (most recent call last):
+  File "C:/Users/Melinda/Desktop/M2T1_SalesPrediction_RyanOxendine.py", line 14, in <module>
+    print('the profit is $', format(print, ',.2f'))
+TypeError: unsupported format string passed to builtin_function_or_method.__format__
+>>> 
+=== RESTART: C:/Users/Melinda/Desktop/M2T1_SalesPrediction_RyanOxendine.py ===
+Enter the Projected sales: 1000
+the profit is $ 230.00
+>>> 
+=== RESTART: C:/Users/Melinda/Desktop/M2T1_SalesPrediction_RyanOxendine.py ===
+Enter the Projected sales: 10000
+the profit is $ 2,300.00
+>>> 
